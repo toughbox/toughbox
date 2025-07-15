30d29a4fce21c213c57e01db3d5d4290a964b1b4
toughbox <nowonstation@gmail.com>
2025-07-15 13:45:39 +0900

    CI/CD test

--- auth/src/main/resources/sql/schema.sql
+++ auth/src/main/resources/sql/schema.sql
@@ -1,13 +1,13 @@
-create table if not exists auth.users (
-    id int not null auto_increment,
-    user_id varchar(45) not null,
-    password text not null,
-    primary key (id)
-);
-
-create table if not exists auth.otp (
-    id int not null auto_increment,
-    user_id varchar(45) not null,
-    otp_code varchar(45) not null,
-    primary key (id)
-);
+--create table if not exists auth.users (
+--    id int not null auto_increment,
+--    user_id varchar(45) not null,
+--    password text not null,
+--    primary key (id)
+--);
+--
+--create table if not exists auth.otp (
+--    id int not null auto_increment,
+--    user_id varchar(45) not null,
+--    otp_code varchar(45) not null,
+--    primary key (id)
+--);
